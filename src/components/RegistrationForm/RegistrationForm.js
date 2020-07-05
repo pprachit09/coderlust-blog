30be4cfa8e43ba07d20d41615acc058b8513366c
pprachit09 <pprachit09@gmail.com>
2020-07-05 19:56:59 -0300

    Added basic Layout

---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+
+const RegistrationForm = () => {
+  return (
+    <div></div>
+  )
+}
+
+RegistrationForm.propTypes = {
+
+}
+
+export default withRouter(RegistrationForm)
